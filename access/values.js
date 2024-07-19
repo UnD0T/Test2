beea8808e1e3762aab8527a0f9ea02ced009bf89
UnD0T <162162090+UnD0T@users.noreply.github.com>
2024-07-19 11:41:00 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,9 +3,9 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "21.07.2004"; // Дата рождения
-var fullname = "Лисенко Микита Олегович" // ФИО
-var name = "Микита" // Имя
+var birthdate = "10.02.2008"; // Дата рождения
+var fullname = "Мартинюк Ілля Євгенович" // ФИО
+var name = "Ілля" // Имя
 
 // Паспорт
 var passport_id = "953482516" // Номер паспорта
@@ -14,11 +14,11 @@ var passport_id = "953482516" // Номер паспорта
 var kpp_id = "9534825160" // ИНН
 
 // Права
-var rights_valid_until = "01.01.2025" // Права "Дійсні до"
+var rights_valid_until = "01.01.2027" // Права "Дійсні до"
 var rights_categories = "A, B" // Права "Категорії"
 var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2025" // Сертификат "Дійсний до"
+var covid_valid_until = "01.01.2027" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
